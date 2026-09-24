6594504c78263e6524b1ad37a2834c2df4c97112
ian cuevas <iancuevas7321@gmail.com>
2026-09-24 18:24:55 -0300

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
 
-const GOOGLE_CLIENT_ID = "4670240614-r3krtco726g4c9k5cehc2rr65n40mkq1.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID =
+  import.meta.env.VITE_GOOGLE_CLIENT_ID ||
+  "4670240614-r3krtco726g4c9k5cehc2rr65n40mkq1.apps.googleusercontent.com";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>,
-)
+);
